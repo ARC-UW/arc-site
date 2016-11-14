1fca7b765c0dd1cf14aadfae196e7d6b42de4a4c
Swojit Mohapatra <neelmohapatra@gmail.com>
2016-11-13 22:00:01 -0800

    Updated map and attempted to fix bug

--- assets/scripts/address.js
+++ assets/scripts/address.js
@@ -1,20 +1,52 @@
-var myCenter = new google.maps.LatLng(47.6567, -122.3152);
+// var myCenter = new google.maps.LatLng(47.6567, -122.3152);
+//
+// function initialize() {
+//     var mapProp = {
+//         center: myCenter,
+//         zoom: 18,
+//         scrollwheel: false,
+//         draggable: true,
+//         mapTypeId: google.maps.MapTypeId.ROADMAP
+//     };
+//
+//     var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+//
+//     var marker = new google.maps.Marker({
+//         position: myCenter,
+//     });
+//
+//     marker.setMap(map);
+// }
+// google.maps.event.addDomListener(window, 'load', initialize);
+var map;
 
-function initialize() {
-    var mapProp = {
-        center: myCenter,
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {
+            lat: 47.6567,
+            lng: -122.3152
+        },
         zoom: 18,
-        scrollwheel: false,
         draggable: true,
+        scrollwheel: false,
         mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-
-    var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
-
-    var marker = new google.maps.Marker({
-        position: myCenter,
     });
-
-    marker.setMap(map);
+    marker = new google.maps.Marker({
+        map: map,
+        draggable: true,
+        animation: google.maps.Animation.DROP,
+        position: {
+            lat: 47.6567,
+            lng: -122.3152
+        }
+    });
+    marker.addListener('click', toggleBounce);
 }
-google.maps.event.addDomListener(window, 'load', initialize);
+
+function toggleBounce() {
+    if (marker.getAnimation() !== null) {
+        marker.setAnimation(null);
+    } else {
+        marker.setAnimation(google.maps.Animation.BOUNCE);
+    }
+}
