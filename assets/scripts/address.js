7938355db21a67564147449ce5035a887141b6f0
Swojit Mohapatra <neelmohapatra@gmail.com>
2016-12-14 17:04:07 -0800

    Marker centered perfectly and aligned text under images

--- assets/scripts/address.js
+++ assets/scripts/address.js
@@ -16,11 +16,21 @@ function initMap() {
         draggable: true,
         animation: google.maps.Animation.DROP,
         position: {
-            lat: 47.6567,
-            lng: -122.3152
+            lat: 47.6566891600,
+            lng: -122.3152107288
         }
     });
     marker.addListener('click', toggleBounce);
+
+    // Manually adjusted the marker and found the center
+    // Should be a cleaner way to do this
+    google.maps.event.addListener(marker, 'dragend', function(){
+        console.log('Marker dropped: Current Lat: ' + evt.latLng.lat().toFixed(10) + ' Current Lng: ' + evt.latLng.lng().toFixed(10));
+    });
+
+    google.maps.event.addListener(marker, 'dragstart', function(){
+        console.log("Currently dragging marker...");
+    });
 }
 
 function toggleBounce() {
